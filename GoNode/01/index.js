e39b72fcbf3e7b96d1233541c742f941f488fb09
Luiz Cunha <you@example.com>
2019-10-16 11:21:19 -0400

    Aprendendo sobre variaveis dentro da url

--- GoNode/01/index.js
+++ GoNode/01/index.js
@@ -10,4 +10,14 @@ app.get("/", (req, res) => {
   return res.send("res");
 });
 
+app.get("/variavel", (req, res) => {
+  return res.send(`Bem vindo ${req.query.name}`);
+});
+
+app.get("/json", (req, res) => {
+  return res.json({
+    mensagem: `Bem vindo ${req.query.name}`
+  });
+});
+
 app.listen(4000);
