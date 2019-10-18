e7a0394577b48c8c4165924316fd9f2270a09983
Luiz Cunha <you@example.com>
2019-10-18 12:15:37 -0400

    utilizando nunkucks para mostar listagem

--- GoNode/01/index.js
+++ GoNode/01/index.js
@@ -1,32 +1,20 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const app = express();
 
-const logmiddleaware = (req, res, next) => {
-  console.log("midware");
-  // para que nção bloqueie a chamada de nova ação
-  return next();
-};
-
-// Todas as coisas vao passar por esse use
-app.use(logmiddleaware);
-
-app.get("/", logmiddleaware, (req, res) => {
-  console.log("Req:");
-  console.log(req);
-  console.log("Res:");
-  console.log(res);
-  return res.send("res");
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
 });
 
-app.get("/variavel", (req, res) => {
-  return res.send(`Bem vindo ${req.query.name}`);
-});
+app.set("view engine", "njk");
 
-app.get("/json", (req, res) => {
-  return res.json({
-    mensagem: `Bem vindo ${req.query.name}`
-  });
+const users = ["Luiz Cunha", "Thiago machele"];
+
+app.get("/", (req, res) => {
+  return res.render(`list`, { users });
 });
 
 app.listen(4000);
