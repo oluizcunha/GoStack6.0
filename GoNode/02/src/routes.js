6184d136ed5c5e27f86b81ac153741829ae4802d
Luiz Cunha <you@example.com>
2019-10-21 13:16:57 -0400

    modularizando nunjuncks para separar conteudos da pagina

--- GoNode/02/src/routes.js
+++ GoNode/02/src/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  return res.send("criando server inicial");
+  return res.render("auth/signup");
 });
 
 module.exports = routes;
