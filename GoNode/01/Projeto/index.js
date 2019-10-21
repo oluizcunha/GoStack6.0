becd4e349451d80f75fecee5e67c1f82def2df36
Luiz Cunha <you@example.com>
2019-10-21 10:14:48 -0400

    Copiando resposta desafio

--- GoNode/01/Projeto/index.js
+++ GoNode/01/Projeto/index.js
@@ -12,15 +12,41 @@ nunjucks.configure("views", {
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "njk");
 
-const users = ["Luiz Cunha", "Thiago machele"];
+const checkAgeQueryParam = (req, res, next) => {
+  const { age } = req.query
 
-app.get("/", (req, res) => {});
+  if (!age) {
+    return res.redirect('/')
+  }
 
-app.post("/check", (req, res, next) => {});
+  return next()
+}
 
-app.get("/major", (req, res) => {});
+app.get('/', (req, res) => {
+  return res.render('start')
+})
 
-app.get("/minor", (req, res) => {});
+app.get('/major', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+
+  return res.render('major', { age })
+})
+
+app.get('/minor', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+
+  return res.render('minor', { age })
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+
+  if (age >= 18) {
+    return res.redirect(`/major?age=${age}`)
+  } else {
+    return res.redirect(`/minor?age=${age}`)
+  }
+})
 
 app.listen(4000);
 
