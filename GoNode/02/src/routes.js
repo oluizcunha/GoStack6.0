cd88f673c99144575df0d12ba21a5dbc27510f5e
luizcunha <luizotcunha@gmail.com>
2019-10-21 14:53:38 -0400

    erro no inicio

--- GoNode/02/src/routes.js
+++ GoNode/02/src/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  return res.render("auth/signup");
+  return res.render("_auth/signup");
 });
 
 module.exports = routes;
