1c4d787a82763b7d49976d3f510f21b1efca1550
Luiz Cunha <you@example.com>
2019-10-21 11:08:13 -0400

    configurando sequelize

---
+++ GoNode/02/src/config/database.js
@@ -0,0 +1,13 @@
+module.exprests = {
+  dialect: "postgres",
+  host: "127.0.0.1",
+  username: "docker",
+  password: "docker",
+  database: "gonodemodulo2",
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
