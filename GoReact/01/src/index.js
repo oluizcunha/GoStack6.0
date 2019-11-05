55c0474e2832c3a5b86aa69daac14b1afa7ffcde
luizcunha <luizotcunha@gmail.com>
2019-11-05 19:32:04 -0400

    State e imutabilidade'

--- GoReact/01/src/index.js
+++ GoReact/01/src/index.js
@@ -13,33 +13,26 @@ class Button extends Component {
   };
 
   render() {
-    return (
-      <a href="" onClick={this.props.onClick}>
-        {this.props.children}
-      </a>
-    );
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
 
 class App extends Component {
-  handleClick() {
-    alert("botão clicado");
-  }
+  state = {
+    contador: 1
+  };
 
+  //Se não utilizarmos uma arrow funcion, o this está referenciando a função. Com isso ele não encontra os componentes da classe mae
+  handleClick = () => {
+    //Não é possivel alterar valor do estado, apenas pode sobrepor com  afunção setState (replace)
+    this.setState({ contador: this.state.contador + 1 });
+  };
+  // Quando o state é alterando, o render é executando. Fazendo com que atualize os dados da pagina
   render() {
     return (
       <Fragment>
-        <h1>Oi Brasil</h1>
-        {/* /Mostrando warning no console pois foi definido o proptypes requerido */}
-        <Button />
-
-        {/* Criou uma arrow funcion pra retornar um alerta */}
-        <Button
-          onClick={() => {
-            alert("botao 1");
-          }}
-        />
-
+        <h1>Eai</h1>
+        <h2>{this.state.contador}</h2>
         <Button onClick={this.handleClick}>Enviar</Button>
       </Fragment>
     );
