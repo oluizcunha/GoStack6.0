1c9e3e1eedbdc74d4035d53de6a3ed8a6ee90120
Luiz Cunha <luizotcunha@gmail.com>
2019-11-05 15:00:59 -0400

    metade da aula 10

--- GoReact/01/src/index.js
+++ GoReact/01/src/index.js
@@ -1,7 +1,17 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 import { render } from "react-dom";
 
 class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+
   render() {
     return (
       <a href="" onClick={this.props.onClick}>
@@ -20,6 +30,16 @@ class App extends Component {
     return (
       <Fragment>
         <h1>Oi Brasil</h1>
+        {/* /Mostrando warning no console pois foi definido o proptypes requerido */}
+        <Button />
+
+        {/* Criou uma arrow funcion pra retornar um alerta */}
+        <Button
+          onClick={() => {
+            alert("botao 1");
+          }}
+        />
+
         <Button onClick={this.handleClick}>Enviar</Button>
       </Fragment>
     );
