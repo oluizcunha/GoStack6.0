3bb6320d1d745c43392dea9169f662da7844bbb5
Luiz Cunha <luizotcunha@gmail.com>
2019-11-06 09:56:35 -0400

    Configurando prettier e eslint - React 21

--- GoReact/01/src/Button.js
+++ GoReact/01/src/Button.js
@@ -1,19 +1,19 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // Deixando componente em função, props deverão ser passadas como função
 
-const Button = props => (
+const Button = (props) => (
   <button onClick={props.onClick}>{props.children}</button>
 );
 
 Button.defaultProps = {
-  children: "Salvar"
+  children: 'Salvar',
 };
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.string
+  children: PropTypes.string,
 };
 
 export default Button;
--- GoReact/01/src/index.js
+++ GoReact/01/src/index.js
@@ -1,43 +1,45 @@
-import React, { Component, Fragment } from "react";
-import { render } from "react-dom";
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-import Button from "./Button";
+import Button from './Button';
 
-import "./style.scss";
+import './style.scss';
+
 class App extends Component {
   state = {
-    contador: 1
+    contador: 1,
   };
 
-  //Se não utilizarmos uma arrow funcion, o this está referenciando a função. Com isso ele não encontra os componentes da classe mae
+  // Se não utilizarmos uma arrow funcion, o this está referenciando a função. Com isso ele não encontra os componentes da classe mae
   handleClick = () => {
-    //Não é possivel alterar valor do estado, apenas pode sobrepor com  afunção setState (replace)
+    // Não é possivel alterar valor do estado, apenas pode sobrepor com  afunção setState (replace)
     this.setState({ contador: this.state.contador + 1 });
 
-    //A alteração do state entra em fila e demora algum tempo para ser processada, por isso ele exibe o valor antigo.
-    console.log("Print antes de processar " + this.state.contador);
+    // A alteração do state entra em fila e demora algum tempo para ser processada, por isso ele exibe o valor antigo.
+    console.log('Print antes de processar ' + this.state.contador);
 
-    //Umas das formas para que ele execute obrigatoriamente depois do processamento de alterar o stado é
-    //criando uma função de callback:
+    // Umas das formas para que ele execute obrigatoriamente depois do processamento de alterar o stado é
+    // criando uma função de callback:
 
     this.setState({ contador: this.state.contador + 1 }, () => {
-      console.log("Print depois de processar " + this.state.contador);
+      console.log('Print depois de processar ' + this.state.contador);
     });
 
     // Para acessar o state que está presente na fila, deve-se atribuir em uma função (fazendo alteração instantanea)
-    this.setState(state => ({ contador: this.state.contador + 1 }));
-    console.log("Print com alteração instantanea" + contador);
+    this.setState((state) => ({ contador: this.state.contador + 1 }));
+    console.log('Print com alteração instantanea' + contador);
   };
+
   // Quando o state é alterando, o render é executando. Fazendo com que atualize os dados da pagina
   render() {
     return (
-      <Fragment>
+      <>
         <h1>Eai</h1>
         <h2>{this.state.contador}</h2>
         <Button onClick={this.handleClick}>Enviar</Button>
-      </Fragment>
+      </>
     );
   }
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById('app'));
